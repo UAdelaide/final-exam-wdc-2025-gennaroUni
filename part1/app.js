31e51766711398cf0f97b13af3881bc5da7c7075
gennaroUni <gennaro.demarco@student.adelaide.edu.au>
2025-06-20 03:08:37 +0000

    Database created at runtime

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql2/promise');
+const fs = require('fs/promises');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+(async () => {
+  try {
+
+    // Read the full SQL file
+    const db = await fs.readFile('./dogwalks.sql', 'utf8');
+
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      multipleStatements: true
+    });
+
+    // Execute the whole dogwalks.sql file (it creates & uses the database itself)
+    await connection.query(db);
+
+    console.log('Database created and initialized from dogwalks.sql file');
+
+    await connection.end();
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+})();
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
